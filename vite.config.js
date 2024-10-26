d9a492ae99a743bf79affb6c091a0f6e477d5f61
sandeeprana0512 <145690424+sandeeprana0512@users.noreply.github.com>
2024-10-26 11:55:59 +0530

    Your commit message describing the changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-export default {
+export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-hot-toast']
-    }
-  }
-};
+      external: ['react-hot-toast'], // Add this line
+    },
+  },
+})
